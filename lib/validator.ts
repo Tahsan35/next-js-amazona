029f1b1d311c77b6f15c909c2512b1fa32451f18
Tahan Shakil <tahsanknmas14@gmail.com>
2025-05-15 23:57:42 +0600

    check

--- lib/validator.ts
+++ lib/validator.ts
@@ -1,4 +1,4 @@
-import { z } from "zod";
+import { z } from "zod/v4";
 import { formatNumberWithDecimal } from "./utils";
 
 // Common
@@ -13,6 +13,20 @@ const Price = (field: string) =>
       (value) => /^\d+(\.\d{2})?$/.test(formatNumberWithDecimal(value)),
       `${field} must have exactly two decimal places (e.g., 49.99)`
     );
+
+export const ReviewInputSchema = z.object({
+  product: MongoId,
+  user: MongoId,
+  isVerifiedPurchase: z.boolean(),
+  title: z.string().min(1, "Title is required"),
+  comment: z.string().min(1, "Comment is required"),
+  rating: z.coerce
+    .number()
+    .int()
+    .min(1, "Rating must be at least 1")
+    .max(5, "Rating must be at most 5"),
+});
+
 //product
 export const ProductInputSchema = z.object({
   name: z.string().min(3, "Name must be at least 3 characters"),
@@ -42,7 +56,7 @@ export const ProductInputSchema = z.object({
   ratingDistribution: z
     .array(z.object({ rating: z.number(), count: z.number() }))
     .max(5),
-  reviews: z.array(z.string()).default([]),
+  reviews: z.array(ReviewInputSchema).default([]),
   numSales: z.coerce
     .number()
     .int()
